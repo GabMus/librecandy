7cac1abbd65c2ad39b8d99d29ec716355ad20b53
GabMus <emaildigabry@gmail.com>
2017-05-12 19:11:04 +0200

    finalized sample UNSAFE api

--- src/node-api-backend/router.js
+++ src/node-api-backend/router.js
@@ -3,6 +3,8 @@ var router = express.Router(); // new instance of express router
 var models = require('./models/librecandyModels.js')
 var bodyParser = require('body-parser');
 
+const API_SUCCESS_MSG = {message: 'success', error: null};
+
 router.use(function(req, res, next) {
     //logging
     console.log('Something happening');
@@ -25,7 +27,7 @@ router.route('/users').post(function(req, res) {
             res.json(err);
             return; //failsafe, dont continue
         }
-        res.json({message: 'success', error: null});
+        res.json(API_SUCCESS_MSG);
     });
 }).get(function(req, res) {
     models.User.find(function(err, users){
@@ -45,14 +47,43 @@ router.route('/users/:username').get(function(req, res){
         }
         res.json(user);
     });
-}).put(function(req,res){ //TODO: finish this, this is curerntly wrong
+}).put(function(req,res) {
     models.User.find({'username': req.params.username}, function(err, user) {
         if (err) {
             res.json(err);
             return;
         }
-        res.json(user);
+
+        user = user[0] //User.find returns a list, can't know username is unique
+
+        if (req.body.realname) {
+            user.realname = req.body.realname;
+        }
+        // if avatar
+        if (req.body.bio) {
+            user.bio = req.body.bio;
+        }
+        console.log('test');
+        console.log(user);
+        user.save(function(err) {
+            if (err) {
+                res.json(err);
+                return; //failsafe, dont continue
+            }
+            res.json(API_SUCCESS_MSG);
+        });
     });
+}).delete(function(req, res) { // NOTE: VERY UNSAFE, DON'T LEAVE LIKE THIS IN PRODUCTION
+    models.User.remove(
+        {username: req.params.username},
+        function(err, user) {
+            if (err) {
+                res.json(err);
+                return; //failsafe, dont continue
+            }
+            res.json(API_SUCCESS_MSG);
+        }
+    );
 });
 
 module.exports = router;

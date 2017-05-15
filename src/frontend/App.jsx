8d9671386fa61563b8b15ca683a15266ab5c9b8d
bladrim <mauromzz93@gmail.com>
2017-05-15 10:29:24 +0200

    Non working material-components/react hello world

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <div>
-            Hello World!!!
-         </div>
-      );
+      <h2 class="mdc-typography--display2">Hello, Material Components!</h2>
+      <div class="mdc-textfield" data-mdc-auto-init="MDCTextfield">
+        <input type="text" class="mdc-textfield__input" id="demo-input">
+        <label for="demo-input" class="mdc-textfield__label">Tell us how you feel!</label>
+      </div>
    }
 }
 

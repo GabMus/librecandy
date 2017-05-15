398b1cc655bc25538c5e91d41062f9b425e1d9a5
federico <federico.grasso@protonmail.com>
2017-05-15 10:41:04 +0200

    Remove Material Components with react

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-      <h2 class="mdc-typography--display2">Hello, Material Components!</h2>
-      <div class="mdc-textfield" data-mdc-auto-init="MDCTextfield">
-        <input type="text" class="mdc-textfield__input" id="demo-input">
-        <label for="demo-input" class="mdc-textfield__label">Tell us how you feel!</label>
-      </div>
+         <div>
+            Hello World!!!
+         </div>
+      );
    }
 }
 

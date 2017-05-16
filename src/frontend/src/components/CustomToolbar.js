2c83a84016f285457eb1a254c877bc6729f15c4e
Veenz <vzfiletti@gmail.com>
2017-05-16 19:50:25 +0200

    Imported topbar

---
+++ src/frontend/src/components/CustomToolbar.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import SearchBox from './SearchBox';
+
+import '../App.css';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
+class CustomToolbar extends Component {
+
+  render() {
+    return (
+      <div>
+
+      <Toolbar>
+        <ToolbarGroup>
+          <IconMenu
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationMenu />
+              </IconButton>
+            }>
+            <MenuItem primaryText="Download" />
+            <MenuItem primaryText="More Info" />
+          </IconMenu>
+          <ToolbarTitle text="Librecandy"/>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <SearchBox/>
+        </ToolbarGroup>
+      </Toolbar>
+      </div>
+    );
+  }
+}
+
+export default CustomToolbar;

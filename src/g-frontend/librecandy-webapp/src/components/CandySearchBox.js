daafb8ee3ee9f65ff7e72a98e9ea05e419880c3f
Vincenzo Filetti <vzfiletti@gmail.com>
2017-05-29 11:54:59 +0200

    handleKeyPress without fetch implemented

--- src/g-frontend/librecandy-webapp/src/components/CandySearchBox.js
+++ src/g-frontend/librecandy-webapp/src/components/CandySearchBox.js
@@ -1,15 +1,26 @@
 import TextField from 'material-ui/TextField';
-import React from 'react';
+import React, { Component } from 'react';
 
-const CandySearchBox = () =>{
-    return(
-      <div className="CandySearchBox">
-        <TextField
-          hintText="Search..."
-          inputStyle={{color: 'white'}}
-        autoFocus/>
-      </div>
-    );
+class CandySearchBox extends Component {
+
+    handleKeyPress = (e) => {
+      let keyCode = (e.keyCode) ? e.keyCode : e.which;
+      console.log("key press captured");
+      if(keyCode === 13){ //Enter key captured
+        /*Starting fetch and loading data screen*/
+      }
+    }
+    render(){
+      return(
+        <div className="CandySearchBox">
+          <TextField
+            hintText="Search..."
+            inputStyle={{color: 'white'}}
+            onKeyPress={(e) => this.handleKeyPress(e)}
+          autoFocus/>
+        </div>
+      );
+    }
 }
 
 export default CandySearchBox;

aeddb32ef6bb0ad72bfc1f7356b2b0780fa21337
GabMus <emaildigabry@gmail.com>
2017-05-30 08:28:03 +0200

    added startproject script, little improvements to search frontend

--- src/g-frontend/librecandy-webapp/src/components/CandySearch.js
+++ src/g-frontend/librecandy-webapp/src/components/CandySearch.js
@@ -1,28 +1,32 @@
-import TextField from 'material-ui/TextField';
 import React, {Component} from 'react';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import TextField from 'material-ui/TextField';
 import ActionSearchIcon from 'material-ui/svg-icons/action/search';
 import IconButton from 'material-ui/IconButton';
 import CandySearchBox from './CandySearchBox';
 
 class CandySearch extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      isSearchActive: false
+    constructor(props){
+        super(props);
+        this.props=props;
+        this.state = {
+            isSearchActive: false
+        }
     }
-  }
 
-  render(){
-    if(this.state.isSearchActive){
-      /*Manage key press for fetch request*/
+    render(){
+
+        if(this.state.isSearchActive){
+            /*Manage key press for fetch request*/
+        }
+        return(
+            <div
+                onTouchTap={() => this.setState({isSearchActive: true})}
+                onBlur={() => this.setState({isSearchActive: false})}>
+                {(this.state.isSearchActive === false) ? <IconButton><ActionSearchIcon color={'white'}/></IconButton> : <CandySearchBox/>}
+            </div>
+        );
     }
-    return(
-      <div onClick={() => this.setState({isSearchActive: true})}
-           onBlur={() => this.setState({isSearchActive: false})}>
-        {(this.state.isSearchActive === false) ? <IconButton><ActionSearchIcon color={'white'}/></IconButton> : <CandySearchBox/>}
-      </div>
-    );
-  }
 }
 
-export default CandySearch;
+export default muiThemeable() (CandySearch);

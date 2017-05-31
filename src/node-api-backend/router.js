74807a3e3ab5b61ab5f6c1d82a3c5a5e4de37c4b
federico <federico.grasso@protonmail.com>
2017-05-31 20:32:19 +0200

    Deployed avatar post request

--- src/node-api-backend/router.js
+++ src/node-api-backend/router.js
@@ -328,7 +328,7 @@ router.route('/users/:username/avatar').post(auth.isAuthenticated,
                       }
                     });
                     console.log('avatar: imagemagick:' + stdout);
-                    user.avatar = user_avatar_path;
+                    user.avatar = user_avatar_dir + '/avatar.png';
                     // remove file in /tmp
                     fs.unlink(req.file.path, function(err) {
                         if (err) console.log(err);
